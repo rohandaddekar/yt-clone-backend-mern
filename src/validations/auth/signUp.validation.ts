5dd745c786d4d40ba9321778efdc270a843c9925
Rohan Daddekar <rohandaddekar9@gmail.com>
2025-02-27 15:03:50 +0530

    update - signUp validation

--- src/validations/auth/signUp.validation.ts
+++ src/validations/auth/signUp.validation.ts
@@ -3,10 +3,17 @@ import { body } from "express-validator";
 const validateSignUp = [
   body("firstName").trim().notEmpty().withMessage("First name is required"),
   body("lastName").trim().notEmpty().withMessage("Last name is required"),
-  body("email").trim().isEmail().withMessage("Email is required"),
+  body("email")
+    .trim()
+    .normalizeEmail()
+    .isEmail()
+    .withMessage("Valid email is required"),
   body("password")
+    .trim()
     .isLength({ min: 6 })
     .withMessage("Password must be at least 6 characters long"),
+  body("bio").optional().trim(),
+  body("isActive").optional().isBoolean(),
 ];
 
 export default validateSignUp;
